18f7dd3d6a8d3e6a04ddb0a1eebcd1b46cdf7846
barilliumcode <williammsmiths@icloud.com>
2026-02-26 16:16:49 -0500

    Updated Model.R

--- Model.R
+++ Model.R
@@ -12,8 +12,11 @@
 # 5.) Competitive native condition: A population of C. irus with both 
 ##### L. perennis and L. polyphyllus as non-zero numbers. Additionally,  
 ##### L. perennis have adapted to better compete with L. polyphyllus.
+# 6.) Altered area eaten condition: A population of C. irus with both 
+##### L. perennis and L. polyphyllus as non-zero numbers. Additionally,  
+##### C. irus eat more L. polyphyllus, as observed in the feeding experiments.
 
-# Adapted from Strimas-Mackey, M. (2020). Lotka-Volterra Predator Prey Model. https://strimas.com/post/lotka-volterra/
+# Adapted from https://strimas.com/post/lotka-volterra/
 
 #---------- Loading packages --------------------
 install.packages("tidyverse") #pivot_longer to reshape data
@@ -60,10 +63,9 @@ ggplot(long_df, aes(time, Value, shape=Variable, colour=Variable))+
   theme(legend.position="none")
 
 # ------------------- 
-
-# Scenario 1 Normal Condition
-pars <- c(decay = 0.7, Sbenefit = 0.000014, Sgrowth = 1, Sconsumed = 0.00018, 
-          k = 80000, Lbenefit = 0.000005, Lgrowth = 1.5, Lconsumed = 0.00018)
+# Scenario 1 Normal Condition a.k.a. equilibrium
+pars <- c(decay = 0.5, Sbenefit = 0.00001, Sgrowth = 1333, Sconsumed = 0.2, 
+          k = 80000, Lbenefit = 0.0000033, Lgrowth = 1400, Lconsumed = 0.2)
 init <- c(x = 2500, y = 50000, z=0) #sets initial states for lv_results1
 time <- seq(0, 50, by = 1) #sets time sequence for lv_results1
 deriv <- function(t, state, pars) {
@@ -80,10 +82,16 @@ long_df1<-pivot_longer(lv_results1,
                       cols=matches("x|y|z"),
                       names_to="Variable",
                       values_to="Value")
+ggplot(long_df1, aes(time, Value, shape=Variable, colour=Variable))+
+  geom_point()+
+  ylim(0,80000)+
+  theme_classic()+
+  labs(x="Time", y= "Populations")+
+  theme(legend.position="none", text = element_text(family = "Helvetica"))
 
 # Scenario 2 Introduced Condition
-pars <- c(decay = 0.7, Sbenefit = 0.000014, Sgrowth = 1, Sconsumed = 0.00018, 
-          k = 80000, Lbenefit = 0.000005, Lgrowth = 1.5, Lconsumed = 0.00018)
+pars <- c(decay = 0.5, Sbenefit = 0.00001, Sgrowth = 1333, Sconsumed = 0.2, 
+          k = 80000, Lbenefit = 0.0000033, Lgrowth = 1400, Lconsumed = 0.2)
 init <- c(x = 2500, y = 50000, z=100) #sets initial states for lv_results2
 time <- seq(0, 50, by = 1) #sets time sequence for lv_results2
 deriv <- function(t, state, pars) {
@@ -100,10 +108,15 @@ long_df2<-pivot_longer(lv_results2,
                        cols=matches("x|y|z"),
                        names_to="Variable",
                        values_to="Value")
+ggplot(long_df2, aes(time, Value, shape=Variable, colour=Variable))+
+  geom_point()+
+  theme_classic()+
+  labs(x="Time", y= "Populations")+
+  theme(legend.position="none", text = element_text(family = "Helvetica"))
 
 # Scenario 3 Inverted Condition
-pars <- c(decay = 0.7, Sbenefit = 0.000014, Sgrowth = 1, Sconsumed = 0.00018, 
-          k = 80000, Lbenefit = 0.000005, Lgrowth = 1.5, Lconsumed = 0.00018)
+pars <- c(decay = 0.5, Sbenefit = 0.00001, Sgrowth = 1333, Sconsumed = 0.2, 
+          k = 80000, Lbenefit = 0.0000033, Lgrowth = 1400, Lconsumed = 0.2)
 init <- c(x = 2500, y = 0, z=50000) #sets initial states for lv_results3
 time <- seq(0, 50, by = 1) #sets time sequence for lv_results3
 deriv <- function(t, state, pars) {
@@ -120,12 +133,17 @@ long_df3<-pivot_longer(lv_results3,
                        cols=matches("x|y|z"),
                        names_to="Variable",
                        values_to="Value")
+ggplot(long_df3, aes(time, Value, shape=Variable, colour=Variable))+
+  geom_point()+
+  theme_classic()+
+  labs(x="Time", y= "Populations")+
+  theme(legend.position="none", text = element_text(family = "Helvetica"))
 
 # Scenario 4 Introduced benefits butterfly condition
-pars <- c(decay = 0.7, Sbenefit = 0.000014, Sgrowth = 1, Sconsumed = 0.00018, 
-          k = 80000, Lbenefit = 0.000010, Lgrowth = 1.5, Lconsumed = 0.00018)
+pars <- c(decay = 0.5, Sbenefit = 0.00001, Sgrowth = 1333, Sconsumed = 0.2, 
+          k = 80000, Lbenefit = 0.000008, Lgrowth = 1400, Lconsumed = 0.2)
 init <- c(x = 2500, y = 50000, z=100) #sets initial states for lv_results4
-time <- seq(0, 50, by = 1) #sets time sequence for lv_results4
+time <- seq(0, 500, by = 1) #sets time sequence for lv_results4
 deriv <- function(t, state, pars) {
   with(as.list(c(state, pars)), {
     d_x <- -decay*x + Sbenefit*x*y+Lbenefit*x*z
@@ -140,82 +158,59 @@ long_df4<-pivot_longer(lv_results4,
                        cols=matches("x|y|z"),
                        names_to="Variable",
                        values_to="Value")
-
-# Scenario 5 Competitive native condition
-pars <- c(decay = 0.7, Sbenefit = 0.000014, Sgrowth = 1.4, Sconsumed = 0.00018, 
-          k = 80000, Lbenefit = 0.000005, Lgrowth = 1.5, Lconsumed = 0.00018)
-init <- c(x = 2500, y = 50000, z=100) #sets initial states for lv_results5a
-time <- seq(0, 50, by = 1) #sets time sequence for lv_results5a
-deriv <- function(t, state, pars) {
-  with(as.list(c(state, pars)), {
-    d_x <- -decay*x + Sbenefit*x*y+Lbenefit*x*z
-    d_y <- Sgrowth*y*(1-((y+z)/k)) - Sconsumed*x*y
-    d_z <- Lgrowth*z*(1-((y+z)/k)) - Lconsumed*x*z
-    return(list(c(x = d_x, y = d_y, z = d_z)))})}
-lv_results5a <- ode(init, time, deriv, pars)
-class(lv_results5a) #pivot_ function doesnt work with matrix
-lv_results5a=as.data.frame(lv_results5a) #convert to df
-class(lv_results5a) #check if df now
-long_df5a<-pivot_longer(lv_results5a,
-                       cols=matches("x|y|z"),
-                       names_to="Variable",
-                       values_to="Value")
-
-# Scenario 5 Competitive native condition **further time steps**
-pars <- c(decay = 0.7, Sbenefit = 0.000014, Sgrowth = 1.4, Sconsumed = 0.00018, 
-          k = 80000, Lbenefit = 0.000005, Lgrowth = 1.5, Lconsumed = 0.00018)
-init <- c(x = 2500, y = 50000, z=100) #sets initial states for lv_results5b
-time <- seq(0, 200, by = 1) #sets time sequence for lv_results5b
-deriv <- function(t, state, pars) {
-  with(as.list(c(state, pars)), {
-    d_x <- -decay*x + Sbenefit*x*y+Lbenefit*x*z
-    d_y <- Sgrowth*y*(1-((y+z)/k)) - Sconsumed*x*y
-    d_z <- Lgrowth*z*(1-((y+z)/k)) - Lconsumed*x*z
-    return(list(c(x = d_x, y = d_y, z = d_z)))})}
-lv_results5b <- ode(init, time, deriv, pars)
-class(lv_results5b) #pivot_ function doesnt work with matrix
-lv_results5b=as.data.frame(lv_results5b) #convert to df
-class(lv_results5b) #check if df now
-long_df5b<-pivot_longer(lv_results5b,
-                       cols=matches("x|y|z"),
-                       names_to="Variable",
-                       values_to="Value")
-
-#---------- Graphing all scenarios together
-par(mfrow=c(2,3)) #not working??
-ggplot(long_df1, aes(time, Value, shape=Variable, colour=Variable))+
-  geom_point()+
-  ylim(0,80000)+
-  theme_classic()+
-  labs(x="Time", y= "Populations")+
-  theme(legend.position="none", text = element_text(family = "Times New Roman"))
-ggplot(long_df2, aes(time, Value, shape=Variable, colour=Variable))+
-  geom_point()+
-  ylim(0,80000)+
-  theme_classic()+
-  labs(x="Time", y= "Populations")+
-  theme(legend.position="none", text = element_text(family = "Times New Roman"))
-ggplot(long_df3, aes(time, Value, shape=Variable, colour=Variable))+
-  geom_point()+
-  ylim(0,80000)+
-  theme_classic()+
-  labs(x="Time", y= "Populations")+
-  theme(legend.position="none", text = element_text(family = "Times New Roman"))
 ggplot(long_df4, aes(time, Value, shape=Variable, colour=Variable))+
   geom_point()+
   ylim(0,80000)+
   theme_classic()+
   labs(x="Time", y= "Populations")+
-  theme(legend.position="none", text = element_text(family = "Times New Roman"))
-ggplot(long_df5a, aes(time, Value, shape=Variable, colour=Variable))+
+  theme(legend.position="none", text = element_text(family = "Helvetica"))
+
+# Scenario 5 Competitive native condition
+pars <- c(decay = 0.5, Sbenefit = 0.00001, Sgrowth = 1390, Sconsumed = 0.2, 
+          k = 80000, Lbenefit = 0.0000033, Lgrowth = 1400, Lconsumed = 0.2)
+init <- c(x = 2500, y = 50000, z=100) #sets initial states for lv_results5
+time <- seq(0, 50, by = 1) #sets time sequence for lv_results5
+deriv <- function(t, state, pars) {
+  with(as.list(c(state, pars)), {
+    d_x <- -decay*x + Sbenefit*x*y+Lbenefit*x*z
+    d_y <- Sgrowth*y*(1-((y+z)/k)) - Sconsumed*x*y
+    d_z <- Lgrowth*z*(1-((y+z)/k)) - Lconsumed*x*z
+    return(list(c(x = d_x, y = d_y, z = d_z)))})}
+lv_results5 <- ode(init, time, deriv, pars)
+class(lv_results5) #pivot_ function doesnt work with matrix
+lv_results5=as.data.frame(lv_results5) #convert to df
+class(lv_results5) #check if df now
+long_df5<-pivot_longer(lv_results5,
+                       cols=matches("x|y|z"),
+                       names_to="Variable",
+                       values_to="Value")
+ggplot(long_df5, aes(time, Value, shape=Variable, colour=Variable))+
   geom_point()+
-  ylim(0,80000)+
   theme_classic()+
   labs(x="Time", y= "Populations")+
-  theme(legend.position="none", text = element_text(family = "Times New Roman"))
-ggplot(long_df5b, aes(time, Value, shape=Variable, colour=Variable))+
+  theme(legend.position="none", text = element_text(family = "Helvetica"))
+
+# Scenario 6 Altered Area Eaten Condition
+pars <- c(decay = 0.5, Sbenefit = 0.00001, Sgrowth = 1333, Sconsumed = 0.2, 
+          k = 80000, Lbenefit = 0.0000033, Lgrowth = 1400, Lconsumed = 0.21)
+init <- c(x = 2500, y = 50000, z=100) #sets initial states for lv_results6
+time <- seq(0, 500, by = 1) #sets time sequence for lv_results6
+deriv <- function(t, state, pars) {
+  with(as.list(c(state, pars)), {
+    d_x <- -decay*x + Sbenefit*x*y+Lbenefit*x*z
+    d_y <- Sgrowth*y*(1-((y+z)/k)) - Sconsumed*x*y
+    d_z <- Lgrowth*z*(1-((y+z)/k)) - Lconsumed*x*z
+    return(list(c(x = d_x, y = d_y, z = d_z)))})}
+lv_results6 <- ode(init, time, deriv, pars)
+class(lv_results6) #pivot_ function doesnt work with matrix
+lv_results6=as.data.frame(lv_results6) #convert to df
+class(lv_results6) #check if df now
+long_df6<-pivot_longer(lv_results6,
+                       cols=matches("x|y|z"),
+                       names_to="Variable",
+                       values_to="Value")
+ggplot(long_df6, aes(time, Value, shape=Variable, colour=Variable))+
   geom_point()+
-  ylim(0,80000)+
   theme_classic()+
   labs(x="Time", y= "Populations")+
-  theme(legend.position="none", text = element_text(family = "Times New Roman"))
+  theme(legend.position="none", text = element_text(family = "Helvetica"))
